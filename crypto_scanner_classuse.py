b52c01f43b9ec9f961055940be9ed85aba0d045a
mohamedraslan85 <75303084+mohamedraslan85@users.noreply.github.com>
2021-04-08 11:40:05 +0200

    Add files via upload

--- crypto_scanner_classuse.py
+++ crypto_scanner_classuse.py
@@ -128,50 +128,15 @@ from bokeh.plotting import figure
 from bokeh.embed import components
 
 
-app = Flask(__name__)
-#run_with_ngrok(app)   
-
-
-@app.route('/')
-def index():
-    x = np.arange(2, 50, step=.5)
-    y = np.sqrt(x) + np.random.randint(2,50)
-    plot = figure(plot_width=400, plot_height=400,title=None, toolbar_location="below")
-    plot.line(x,y)
-
-    script, div = components(plot)
-    kwargs = {'script': script, 'div': div}
-    kwargs['title'] = 'bokeh-with-flask'    
-    return render_template('index.html', **kwargs)   
-
-if __name__ == '__main__':
-    app.run(debug=True)
 
 import numpy as np 
 from flask import Flask, request, render_template, abort, Response
 from bokeh.plotting import figure
 from bokeh.embed import components
 
-app = Flask(__name__)
 
-@app.route('/')
-def index():
-    x = np.arange(2, 50, step=.5)
-    y = np.sqrt(x) + np.random.randint(2,50)
-    plot = figure(plot_width=400, plot_height=400,title=None, toolbar_location="below")
-    plot.line(x,y)
 
-    script, div = components(plot)
-    kwargs = {'script': script, 'div': div}
-    kwargs['title'] = 'bokeh-with-flask'    
-    return render_template('index.html', **kwargs)   
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
-app.kil
-
-pip install flask_ngrok
+#pip install flask_ngrok
 
 a.get_tweets()
 a.group_tweets()
